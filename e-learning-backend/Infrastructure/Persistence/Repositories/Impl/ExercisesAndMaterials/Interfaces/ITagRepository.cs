459797c162719f13bc117db6d8d0443dfb873bc1
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:31:48 +0200

    feat: implement CRUD operations and interface for Tag entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/ExercisesAndMaterials/Interfaces/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using e_learning_backend.Domain.ExercisesAndMaterials;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface ITagRepository
+{
+    Task<Tag?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Tag>> GetAllAsync();
+    Task AddAsync(Tag tag);
+    Task UpdateAsync(Tag tag);
+    Task DeleteAsync(Guid id);
+    Task<IEnumerable<Tag>> GetByTeacherIdAsync(Guid teacherId);
+}
---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/ExercisesAndMaterials/TagRepository.cs
@@ -0,0 +1,59 @@
+﻿using e_learning_backend.Domain.ExercisesAndMaterials;
+using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories.Impl;
+
+public class TagRepository : ITagRepository
+{
+    private readonly ApplicationContext _context;
+
+    public TagRepository(ApplicationContext context)
+        => _context = context;
+
+    
+    public async Task<Tag?> GetByIdAsync(Guid id)
+        => await _context.Tags
+            .Include(t => t.Teacher)
+            .Include(t => t.Files)
+            .SingleOrDefaultAsync(t => t.Id == id);
+
+    
+    public async Task<IEnumerable<Tag>> GetAllAsync()
+        => await _context.Tags
+            .Include(t => t.Teacher)
+            .Include(t => t.Files)
+            .ToListAsync();
+
+   
+    public async Task AddAsync(Tag tag)
+    {
+        await _context.Tags.AddAsync(tag);
+        await _context.SaveChangesAsync();
+    }
+
+    
+    public async Task UpdateAsync(Tag tag)
+    {
+        _context.Tags.Update(tag);
+        await _context.SaveChangesAsync();
+    }
+
+   
+    public async Task DeleteAsync(Guid id)
+    {
+        var tag = await _context.Tags.FindAsync(id);
+        if (tag != null)
+        {
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    
+    public async Task<IEnumerable<Tag>> GetByTeacherIdAsync(Guid teacherId)
+        => await _context.Tags
+            .Where(t => t.TeacherId == teacherId)
+            .Include(t => t.Files)
+            .ToListAsync();
+}
