cfda9a0c376b0aebf83b9c7c334a2c83e2c7bc7c
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:30:17 +0200

    feat: implement CRUD operations and interface for ExerciseResource entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/ExercisesAndMaterials/Interfaces/IExerciseResourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.ExercisesAndMaterials;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface IExerciseResourceRepository
+{
+    Task<ExerciseResource?> GetByIdsAsync(Guid exerciseId, Guid fileId);
+    Task<IEnumerable<ExerciseResource>> GetAllAsync();
+    Task AddAsync(ExerciseResource resource);
+    Task UpdateAsync(ExerciseResource resource);
+    Task DeleteAsync(Guid exerciseId, Guid fileId);
+}
