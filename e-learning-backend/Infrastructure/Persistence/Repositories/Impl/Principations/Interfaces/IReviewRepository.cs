95b463ae559e47d1848ec57a7cb7b9ce4a3fa098
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:31:42 +0200

    feat: implement CRUD operations and interface for Review entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Principations/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using e_learning_backend.Domain.Participations;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface IReviewRepository
+{
+    Task<Review?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Review>> GetAllAsync();
+    Task AddAsync(Review review);
+    Task UpdateAsync(Review review);
+    Task DeleteAsync(Guid id);
+    Task<Review?> GetByParticipationIdAsync(Guid userId, Guid courseId);
+}
