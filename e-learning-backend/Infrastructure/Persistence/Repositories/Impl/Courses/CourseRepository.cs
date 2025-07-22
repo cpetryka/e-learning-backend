6307d0ef6b38049084d34173d49d2f3c4a6967a0
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:29:17 +0200

    feat: implement CRUD operations and interface for Course entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Courses/CourseRepository.cs
@@ -0,0 +1,53 @@
+﻿using e_learning_backend.Domain.Courses;
+using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories.Impl;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly ApplicationContext _context;
+
+    public CourseRepository(ApplicationContext context) => _context = context;
+
+   
+    public async Task<Course?> GetByIdAsync(Guid id)
+        => await _context.Courses
+            .Include(c => c.Teacher)
+            .Include(c => c.Variants)
+            .Include(c => c.Participations)
+            .SingleOrDefaultAsync(c => c.Id == id);
+
+    
+    public async Task<IEnumerable<Course>> GetAllAsync()
+        => await _context.Courses
+            .Include(c => c.Teacher)
+            .Include(c => c.Variants)
+            .Include(c => c.Participations)
+            .ToListAsync();
+
+    
+    public async Task AddAsync(Course course)
+    {
+        await _context.Courses.AddAsync(course);
+        await _context.SaveChangesAsync();
+    }
+
+    
+    public async Task UpdateAsync(Course course)
+    {
+        _context.Courses.Update(course);
+        await _context.SaveChangesAsync();
+    }
+
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+        if (course != null)
+        {
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Courses/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.Courses;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface ICourseRepository
+{
+    Task<Course?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Course>> GetAllAsync();
+    Task AddAsync(Course course);
+    Task UpdateAsync(Course course);
+    Task DeleteAsync(Guid id);
+}
