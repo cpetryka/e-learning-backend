e416233bebabe7cf8e57a9229cffb0c8ca41dcb1
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:28:01 +0200

    feat: implement CRUD operations and interface for ClassStatus entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Classes/Interfaces/IClassStatusRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.Classes.ValueObjects;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories.Impl;
+
+public interface IClassStatusRepository
+{
+    Task<ClassStatus?> GetByIdAsync(Guid id);
+    Task<IEnumerable<ClassStatus>> GetAllAsync();
+    Task AddAsync(ClassStatus status);
+    Task UpdateAsync(ClassStatus status);
+    Task DeleteAsync(Guid id);
+}
