3d1ce94db8eb55a361f1f60463704373168cc6e9
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:29:04 +0200

    feat: implement CRUD operations and interface for CourseLevel entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Courses/Interfaces/ICourseLevelRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.Courses;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface ICourseLevelRepository
+{
+    Task<CourseLevel?> GetByIdAsync(Guid id);
+    Task<IEnumerable<CourseLevel>> GetAllAsync();
+    Task AddAsync(CourseLevel level);
+    Task UpdateAsync(CourseLevel level);
+    Task DeleteAsync(Guid id);
+}
