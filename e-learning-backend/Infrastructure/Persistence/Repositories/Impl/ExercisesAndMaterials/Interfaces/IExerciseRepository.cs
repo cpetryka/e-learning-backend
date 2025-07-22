a4aa8c644412e97fb1aaacfba7b11394c8210329
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:29:58 +0200

    feat: implement CRUD operations and interface for ExerciseRepository entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/ExercisesAndMaterials/Interfaces/IExerciseRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.ExercisesAndMaterials;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface IExerciseRepository
+{
+    Task<Exercise?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Exercise>> GetAllAsync();
+    Task AddAsync(Exercise exercise);
+    Task UpdateAsync(Exercise exercise);
+    Task DeleteAsync(Guid id);
+}
