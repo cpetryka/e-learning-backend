433388ad7b6df55e7880a5a749f098f2555c8d5c
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-07-22 22:28:34 +0200

    feat: implement CRUD operations and interface for CourseCategory entity

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Courses/Interfaces/ICourseCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using e_learning_backend.Domain.Courses;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface ICourseCategoryRepository
+{
+    Task<CourseCategory?> GetByIdAsync(Guid id);
+    Task<IEnumerable<CourseCategory>> GetAllAsync();
+    Task AddAsync(CourseCategory category);
+    Task UpdateAsync(CourseCategory category);
+    Task DeleteAsync(Guid id);
+}
