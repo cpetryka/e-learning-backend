24718137357d106ca5854e197e3d4a3137e0d7dd
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-21 14:07:06 +0200

    feat: create the UsersService with method to get info about the user "aboutMe"

---
+++ e-learning-backend/Infrastructure/Api/Services/Interfaces/IUsersService.cs
@@ -0,0 +1,8 @@
+﻿using e_learning_backend.Infrastructure.Api.DTO;
+
+namespace e_learning_backend.Infrastructure.Security.Impl.Interfaces;
+
+public interface IUsersService
+{
+    Task<AboutMeDTO?> GetAboutMeAsync(Guid userId);
+}
---
+++ e-learning-backend/Infrastructure/Api/Services/UsersService.cs
@@ -0,0 +1,32 @@
+﻿using e_learning_backend.Infrastructure.Api.DTO;
+using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using e_learning_backend.Infrastructure.Security.Impl.Interfaces;
+
+namespace e_learning_backend.Infrastructure.Security.Impl;
+
+public class UsersService : IUsersService
+{
+    private readonly ApplicationContext _context;
+    
+    public UsersService(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<AboutMeDTO?> GetAboutMeAsync(Guid userId)
+    {
+        var user = await _context.Users.FindAsync(userId);
+
+        if (user == null)
+            return null;
+
+        return new AboutMeDTO
+        {
+            Name = user.Name,
+            Surname = user.Surname,
+            Email = user.Email,
+            PhoneNumber = user.Phone,
+            Description = user.AboutMe,
+        };
+    }
+}
