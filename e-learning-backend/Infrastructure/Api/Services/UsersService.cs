e637ed1ce666fb9128e19c6e105495a009315f11
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-21 14:54:15 +0200

    feat: refactor UsersService to use IUsersRepository instead of DbContext directly

--- e-learning-backend/Infrastructure/Api/Services/UsersService.cs
+++ e-learning-backend/Infrastructure/Api/Services/UsersService.cs
@@ -1,21 +1,21 @@
 ﻿using e_learning_backend.Infrastructure.Api.DTO;
-using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using e_learning_backend.Infrastructure.Persistence.Repositories;
 using e_learning_backend.Infrastructure.Security.Impl.Interfaces;
 
 namespace e_learning_backend.Infrastructure.Security.Impl;
 
 public class UsersService : IUsersService
 {
-    private readonly ApplicationContext _context;
+    private readonly IUsersRepository _usersRepository;
     
-    public UsersService(ApplicationContext context)
+    public UsersService(IUsersRepository usersRepository)
     {
-        _context = context;
+        _usersRepository = usersRepository;
     }
     
     public async Task<AboutMeDTO?> GetAboutMeAsync(Guid userId)
     {
-        var user = await _context.Users.FindAsync(userId);
+        var user = await _usersRepository.GetByIdAsync(userId);
 
         if (user == null)
             return null;
