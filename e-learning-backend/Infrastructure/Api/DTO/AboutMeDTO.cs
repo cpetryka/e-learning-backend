457b6d97603018a8a4f432a82e29526f60a90279
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-21 14:08:38 +0200

    feat: add AboutMeDTO and refactor UserService to return formatted user data

---
+++ e-learning-backend/Infrastructure/Api/DTO/AboutMeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace e_learning_backend.Infrastructure.Api.DTO;
+
+public class AboutMeDTO
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Description { get; set; }
+}
