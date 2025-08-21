949059bb31377a616ae920a615f5c47fe51f9e91
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-21 14:10:34 +0200

    feat: add CoursesController endpoint to fetch courses with filtering options

---
+++ e-learning-backend/Infrastructure/Api/CousesController.cs
@@ -0,0 +1,38 @@
+﻿using e_learning_backend.Infrastructure.Api.DTO;
+using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using e_learning_backend.Infrastructure.Security.Impl.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_learning_backend.Infrastructure.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoursesController : ControllerBase
+{
+    private readonly ICoursesService _coursesService;
+
+    public CoursesController(ICoursesService coursesService)
+    {
+        _coursesService = coursesService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CourseWidgetDTO>>> GetCourses(
+        [FromQuery] string[]? categories,
+        [FromQuery] string[]? levels,
+        [FromQuery] string[]? languages,
+        [FromQuery] int? priceFrom,
+        [FromQuery] int? priceTo,
+        [FromQuery] Guid? teacherId)
+    {
+        var courses = await _coursesService.GetCoursesAsync(
+            categories,
+            levels,
+            languages,
+            priceFrom,
+            priceTo,
+            teacherId);
+
+        return Ok(courses);
+    }
+}
