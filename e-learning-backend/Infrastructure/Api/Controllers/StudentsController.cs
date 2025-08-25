8a5a2cb912a371c3de4be534433436b7fd85c7ef
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-25 18:31:55 +0200

    feat: add Students service, repository, and controller to fetch student courses information

---
+++ e-learning-backend/Infrastructure/Api/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using e_learning_backend.Infrastructure.Security.Impl.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_learning_backend.Infrastructure.Api.Controllers;
+
+using e_learning_backend.Infrastructure.Api.DTO;
+using e_learning_backend.Infrastructure.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
+{
+    private readonly IStudentsService _studentsService;
+
+    public StudentsController(IStudentsService studentsService)
+    {
+        _studentsService = studentsService;
+    }
+  
+    [HttpGet("{studentId:guid}")]
+    
+    public async Task<IActionResult> GetStudentWithCourses(Guid studentId)
+    {
+        var student = await _studentsService.GetStudentWithCoursesAsync(studentId);
+
+        if (student == null)
+            return NotFound();
+
+        return Ok(student);
+    }
+}
---
+++ e-learning-backend/Infrastructure/Api/Services/Interfaces/IStudentsService.cs
@@ -0,0 +1,9 @@
+﻿using e_learning_backend.Domain.Users;
+using e_learning_backend.Infrastructure.Api.DTO;
+
+namespace e_learning_backend.Infrastructure.Security.Impl.Interfaces;
+
+public interface IStudentsService 
+{
+    Task<StudentDTO?> GetStudentWithCoursesAsync(Guid studentId);
+}
