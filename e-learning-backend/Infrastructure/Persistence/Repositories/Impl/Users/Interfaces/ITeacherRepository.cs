252ca3427a4bca4d8bdae8df7ed370109582f8a9
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-25 13:29:40 +0200

    feat: create TeacherRepository for managing teacher data access

---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Users/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,8 @@
+﻿using e_learning_backend.Domain.Users;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories;
+
+public interface ITeacherRepository
+{
+    Task<User?> GetTeacherWithDetailsAsync(Guid teacherId);
+}
---
+++ e-learning-backend/Infrastructure/Persistence/Repositories/Impl/Users/TeacherRepository.cs
@@ -0,0 +1,28 @@
+﻿using e_learning_backend.Domain.Users;
+using e_learning_backend.Domain.Users.ValueObjects;
+using e_learning_backend.Infrastructure.Persistence.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_learning_backend.Infrastructure.Persistence.Repositories.Impl;
+
+public class TeacherRepository : ITeacherRepository
+{
+    private readonly ApplicationContext _context;
+
+    public TeacherRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetTeacherWithDetailsAsync(Guid teacherId)
+    {
+        return await _context.Users
+            .Include(u => u.ConductedCourses)
+            .Include(u => u.Availability)
+            .ThenInclude(a => a.TimeSlots)
+            .Where(u => u.Id == teacherId)
+            .Where(u => u.Roles.Any(r => r.RoleName == Role.Teacher.RoleName)) 
+            .FirstOrDefaultAsync();
+    }
+
+}
