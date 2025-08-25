584c97dda79c134f8c30008de27a4b5600e70450
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-08-25 13:31:37 +0200

    feat: create TeacherDTO for teacher details API response

---
+++ e-learning-backend/Infrastructure/Api/DTO/TeacherDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace e_learning_backend.Infrastructure.Api.DTO;
+
+public class TeacherDTO
+{
+    public string Name { get; set; } = default!;
+    public string Surname { get; set; } = default!;
+    public string? Description { get; set; }
+    public List<CourseBriefDTO> CoursesBrief { get; set; } = new();
+    public List<AvailabilityDTO> Availability { get; set; } = new();
+
+    public class CourseBriefDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+
+    public class AvailabilityDTO
+    {
+        public DateOnly Day { get; set; }
+        public List<TimeslotDTO> Timeslots { get; set; } = new();
+    }
+
+    public class TimeslotDTO
+    {
+        public TimeOnly TimeFrom { get; set; }
+        public TimeOnly TimeUntil { get; set; }
+    }
+}
