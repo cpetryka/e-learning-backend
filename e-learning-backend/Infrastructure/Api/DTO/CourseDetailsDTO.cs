893052038f2895e1bd5774a098735e93d1e9427d
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-01 22:58:29 +0200

    feat: add CourseDetailsDTO aggregating course, teacher, reviews and availability

---
+++ e-learning-backend/Infrastructure/Api/DTO/CourseDetailsDTO.cs
@@ -0,0 +1,15 @@
+﻿using e_learning_backend.Infrastructure.Api.DTO;
+
+public class CourseDetailsDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Image { get; set; }
+    public double Rating { get; set; }
+    public string Description { get; set; } = null!;
+    public IEnumerable<CourseWidgetDTO.CourseVariantDTO> Variants { get; set; } = new List<CourseWidgetDTO.CourseVariantDTO>();
+    public TeacherDTO Teacher { get; set; } = null!;
+    public List<TeacherReviewDTO> TeacherReviews { get; set; } = new();
+    public List<TeacherAvailabilityDTO> TeacherAvailability { get; set; } = new();
+    public string? Category { get; set; }
+}
