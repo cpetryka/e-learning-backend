1afdf63d27088cd7ab9b3c33b939b49a930d1fd6
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-01 22:57:18 +0200

    refactor: extract TeacherReviewDTO and TeacherAvailabilityDTO into separate

--- e-learning-backend/Infrastructure/Api/DTO/TeacherDTO.cs
+++ e-learning-backend/Infrastructure/Api/DTO/TeacherDTO.cs
@@ -6,23 +6,11 @@ public class TeacherDTO
     public string Surname { get; set; } = default!;
     public string? Description { get; set; }
     public List<CourseBriefDTO> CoursesBrief { get; set; } = new();
-    public List<AvailabilityDTO> Availability { get; set; } = new();
 
     public class CourseBriefDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = default!;
     }
-
-    public class AvailabilityDTO
-    {
-        public DateOnly Day { get; set; }
-        public List<TimeslotDTO> Timeslots { get; set; } = new();
-    }
-
-    public class TimeslotDTO
-    {
-        public TimeOnly TimeFrom { get; set; }
-        public TimeOnly TimeUntil { get; set; }
-    }
+    
 }
