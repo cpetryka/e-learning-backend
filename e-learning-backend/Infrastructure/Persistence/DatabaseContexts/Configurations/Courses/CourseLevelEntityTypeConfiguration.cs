10de3be82f4ee6aa3b80929b9f4ec28fbd18beb6
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-02 21:55:50 +0200

    feat: add additional CourseLevel entries to DbContext

--- e-learning-backend/Infrastructure/Persistence/DatabaseContexts/Configurations/Courses/CourseLevelEntityTypeConfiguration.cs
+++ e-learning-backend/Infrastructure/Persistence/DatabaseContexts/Configurations/Courses/CourseLevelEntityTypeConfiguration.cs
@@ -10,8 +10,14 @@ public class CourseLevelEntityTypeConfiguration : IEntityTypeConfiguration<Cours
     {
         builder.HasKey(l => l.Id);
 
-        var levelId = Guid.Parse("1dcb1002-ec77-49ea-8f21-56e0caac0824");
+        var beginnerId     = Guid.Parse("1dcb1002-ec77-49ea-8f21-56e0caac0824");
+        var intermediateId = Guid.Parse("2e9d2f0b-7a5f-4c2d-9a1a-8f3f3c2e7b01");
+        var advancedId     = Guid.Parse("5c4b6e1f-3a2d-4f5b-b3a6-2e1f4c7a9d22");
 
-        builder.HasData(new CourseLevel(levelId, "Beginner"));
+        builder.HasData(
+            new CourseLevel(beginnerId, "Beginner"),
+            new CourseLevel(intermediateId, "Intermediate"),
+            new CourseLevel(advancedId, "Advanced")
+        );
     }
 }
