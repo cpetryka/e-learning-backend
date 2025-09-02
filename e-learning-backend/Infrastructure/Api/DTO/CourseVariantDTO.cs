0b0f1cce019e73786c69bcde297f1f076f7571d0
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-02 21:57:45 +0200

    refactor: Update CourseWidgetDTO fields to better match API return types

---
+++ e-learning-backend/Infrastructure/Api/DTO/CourseVariantDTO.cs
@@ -0,0 +1,6 @@
+﻿public class CourseVariantDTO
+{
+    public string LanguageName { get; set; }
+    public string LevelName { get; set; }
+    public decimal Price { get; set; }
+}
--- e-learning-backend/Infrastructure/Api/DTO/CourseWidgetDTO.cs
+++ e-learning-backend/Infrastructure/Api/DTO/CourseWidgetDTO.cs
@@ -7,18 +7,15 @@
     public string? Image { get; set; } // thumbnail (opcjonalne)
     public double? Rating { get; set; }
     public string Description { get; set; }
-    
-    public List<CourseVariantDTO> Variants { get; set; } = new();
+    public decimal MinimumCoursePrice { get; set; } 
+    public decimal MaximumCoursePrice { get; set; } 
+    public string[] LevelVariants { get; set; } = null!; 
+    public string[] LanguageVariants { get; set; } = null!; 
 
     // nauczyciel
     public Guid TeacherId { get; set; }
     public string TeacherName { get; set; }
     public string TeacherSurname { get; set; }
 
-    public class CourseVariantDTO
-    {
-        public string LanguageName { get; set; }
-        public string LevelName { get; set; }
-        public decimal Price { get; set; }
-    }
+  
 }
