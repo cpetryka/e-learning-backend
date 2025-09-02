1e72a9f47df15aa4954d2cebf979791317f076cc
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-02 22:02:48 +0200

    feat: prepare User entity to store profile picture as a Value Object and as a static file

---
+++ e-learning-backend/Domain/Users/ValueObjects/ProfilePicture.cs
@@ -0,0 +1,23 @@
+﻿namespace e_learning_backend.Domain.Users.ValueObjects;
+
+public class ProfilePicture
+{
+    public string FileName { get; private set; }
+    public string FilePath { get; private set; }
+    public DateTime UploadedAt { get; private set; }
+
+    private ProfilePicture() { }
+
+    public ProfilePicture(string fileName, string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+            throw new ArgumentException("File name cannot be empty.", nameof(fileName));
+
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentException("File path cannot be empty.", nameof(filePath));
+        
+        FileName = fileName;
+        FilePath = filePath;
+        UploadedAt = DateTime.UtcNow;
+    }
+}
