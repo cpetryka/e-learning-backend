541619233ea887252522cb185c697e21905a5d98
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-03 17:07:20 +0200

    refactor: replace nested ProfilePictureDTO in TeacherDTO with a simple TeacherProfilePictureUrl property

--- e-learning-backend/Infrastructure/Api/DTO/TeacherDTO.cs
+++ e-learning-backend/Infrastructure/Api/DTO/TeacherDTO.cs
@@ -7,18 +7,12 @@ public class TeacherDTO
     public string Surname { get; set; } = default!;
     public string? Description { get; set; }
     public List<CourseBriefDTO> CoursesBrief { get; set; } = new();
-    public ProfilePictureDTO? ProfilePicture { get; set; }
+    public string? TeacherProfilePictureUrl { get; set; }
 
     public class CourseBriefDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = default!;
-    }
-    public class ProfilePictureDTO
-    {
-        public string FileName { get; set; } = string.Empty;
-        public string Url { get; set; } = string.Empty;
-        public DateTime UploadedAt { get; set; }
     }
     
 }
