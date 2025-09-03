635ba657646e9a97b320d7af9716cb8eaa4854c4
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-03 17:05:04 +0200

    feat: update CoursesService and UserService interfaces for proper class integration

--- e-learning-backend/Infrastructure/Api/Services/Interfaces/ICoursesService.cs
+++ e-learning-backend/Infrastructure/Api/Services/Interfaces/ICoursesService.cs
@@ -1,4 +1,5 @@
 ﻿using e_learning_backend.Domain.Courses;
+using e_learning_backend.Domain.Users.ValueObjects;
 using e_learning_backend.Infrastructure.Api.DTO;
 
 namespace e_learning_backend.Infrastructure.Security.Impl.Interfaces;
@@ -18,4 +19,5 @@ public interface ICoursesService
     Task<IReadOnlyCollection<CourseCategory>> GetAllDistinctCategoriesAsync();
     Task<IReadOnlyCollection<CourseLevel>> GetAllDistinctLevelsAsync();
     Task<IReadOnlyCollection<CourseLanguage>> GetAllDistinctLanguagesAsync();
+    Task<(bool Success, string Message, ProfilePicture? ProfilePicture)> UploadProfilePictureAsync(Guid courseId, Guid userId, IFormFile file);
 }
--- e-learning-backend/Infrastructure/Api/Services/Interfaces/IUsersService.cs
+++ e-learning-backend/Infrastructure/Api/Services/Interfaces/IUsersService.cs
@@ -1,8 +1,11 @@
-﻿using e_learning_backend.Infrastructure.Api.DTO;
+﻿using e_learning_backend.Domain.Users.ValueObjects;
+using e_learning_backend.Infrastructure.Api.DTO;
 
 namespace e_learning_backend.Infrastructure.Security.Impl.Interfaces;
 
 public interface IUsersService
 {
     Task<AboutMeDTO?> GetAboutMeAsync(Guid userId);
+
+    Task<(bool Success, string Message, ProfilePicture? ProfilePicture)> UploadProfilePictureAsync(Guid userId, IFormFile file);
 }
