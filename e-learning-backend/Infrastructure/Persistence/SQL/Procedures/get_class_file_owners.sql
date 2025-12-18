49405390652830644fc92dd5ff6a3c7b68b9ee0e
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-12-18 21:01:32 +0100

    feat: add SQL procedures

---
+++ e-learning-backend/Infrastructure/Persistence/SQL/Procedures/get_class_file_owners.sql
@@ -0,0 +1,21 @@
+﻿create function get_class_file_owners(p_user_id uuid)
+    returns TABLE("Id" uuid, "Name" character varying, "Surname" character varying)
+    language sql
+    as
+$$
+SELECT DISTINCT
+    ownerUser."Id",
+    ownerUser."Name",
+    ownerUser."Surname"
+FROM "FileResources"
+         INNER JOIN "ClassFileResources"
+                    ON "ClassFileResources"."FileResourceId" = "FileResources"."Id"
+         INNER JOIN "Classes"
+                    ON "ClassFileResources"."ClassId" = "Classes"."Id"
+         INNER JOIN "Users" AS ownerUser
+                    ON "FileResources"."UserId" = ownerUser."Id"
+WHERE "Classes"."UserId" = p_user_id;
+$$;
+
+alter function get_class_file_owners(uuid) owner to inzynier;
+
---
+++ e-learning-backend/Infrastructure/Persistence/SQL/Procedures/get_user_file_tags.sql
@@ -0,0 +1,23 @@
+﻿create function get_user_file_tags(_user_id uuid)
+    returns TABLE("TagId" uuid, "TagName" text, "OwnerId" uuid)
+    language sql
+    as
+$$
+SELECT DISTINCT
+    t."Id"      AS "TagId",
+    t."Name"    AS "TagName",
+    t."UserId" AS "OwnerId"
+FROM "FileResources" fr
+         JOIN "ClassFileResources" cfr ON cfr."FileResourceId" = fr."Id"
+         JOIN "Classes" c ON c."Id" = cfr."ClassId"
+         JOIN "Participations" p ON p."CourseId" = c."CourseId"
+         JOIN "TagFiles" tf ON tf."FileResourceId" = fr."Id"
+         JOIN "Tags" t ON t."Id" = tf."TagId"
+WHERE fr."UserId" = _user_id
+   OR p."UserId" = _user_id;
+$$;
+
+alter function get_user_file_tags(uuid) owner to inzynier;
+
+
+
